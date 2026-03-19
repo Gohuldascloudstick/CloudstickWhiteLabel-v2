93bc9f5bdad5d1d1d244883416506af6ff22de72
Ajith-cloudstick <ajith@cloudstick.io>
2026-03-19 12:00:09 +0530

    feat: Introduce and log `VITE_serverId` environment variable.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter } from "react-router-dom"
 import AppRouter from "./router/AppRouter"
-
+const serverid = import.meta.env.VITE_serverId
+console.log(serverid);
 
 const App = () => {
   return (
-   <BrowserRouter>
-   <AppRouter/>
-   </BrowserRouter>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   )
 }
 
